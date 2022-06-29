e52e14a90bf840556415a8d8e6fd3042fe125e58
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-06-29 15:01:32 +0530

    added store

---
+++ brobillionaire-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
+import "./App.css";
+
+function App() {
+	return <div className="App">Bro Billionaire</div>;
+}
+
+export default App;
