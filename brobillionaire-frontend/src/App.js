1480285a4269d9982f68b11507faffcbd52a4bca
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-06-29 15:42:08 +0530

    minor error

--- brobillionaire-frontend/src/App.js
+++ brobillionaire-frontend/src/App.js
@@ -5,10 +5,32 @@ import {
 	Route,
 	Redirect,
 } from "react-router-dom";
+import LoginPage from "./authPages/loginPages/LoginPage";
+import RegisterPage from "./authPages/registerPages/RegisterPage";
+import Dashboard from "./Dashborad/dashboard";
 import "./App.css";
 
 function App() {
-	return <div className="App">Bro Billionaire</div>;
+	return (
+		<>
+			<Router>
+				<Switch>
+					<Route exact path="/login">
+						<LoginPage />
+					</Route>
+					<Route exact path="/register">
+						<RegisterPage />
+					</Route>
+					<Route exact path="/dashboard">
+						<Dashboard />
+					</Route>
+					<Route path="/">
+						<Redirect to="/dashboard" />
+					</Route>
+				</Switch>
+			</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ brobillionaire-frontend/src/authPages/loginPages/LoginPageHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const LoginPageHeader = () => {
+	return (
+		<div>
+			<Typography>Welcome Back!</Typography>
+			<Typography>Happy to see you again!</Typography>
+		</div>
+	);
+};
+
+export default LoginPageHeader;
